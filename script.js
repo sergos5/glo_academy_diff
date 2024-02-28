a5ed8ed6dcfb66a918a6a1a30f9ef2817187db4c
Sergey <sergos5@yandex.ru>
2024-02-28 18:11:46 +0300

    задание третьего урока через объект

--- script.js
+++ script.js
@@ -1,38 +1,43 @@
 'use strict';
 let lang = prompt('На каком языке вывести дни недели?', '"ru" или "en"');
 
-let week = [
-    ['понедельник', 'вторник', 'среда', 'четверг', 'пятница', 'суббота', 'воскресенье'],
-    ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'Sunday']
-];
+const weekDeys = {
+    ru: ['понедельник', 'вторник', 'среда', 'четверг', 'пятница', 'суббота', 'воскресенье'],
+    en: ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'Sunday']
+};
+
 
 let showWeek = function (arr) {
     for (let i=0; i<arr.length; i++)
         console.log(arr[i]);       
 };
+
 //дни недели через if
 if (lang=='ru') {
-    showWeek(week[0]); 
+    showWeek(weekDeys.ru); 
 } else if (lang=='en'){
-    showWeek(week[1]);
+    showWeek(weekDeys.en);
 } else {
     console.log ('Что то пошло не так');
 }
+
 //дни недели через switch-case
 switch (lang) {
     case 'ru':
-        showWeek(week[0]);
+        showWeek(weekDeys.ru);
         break;    
     case 'en':
-        showWeek(week[1]);
+        showWeek(weekDeys.en);
         break;
     default:
         console.log('Что то пошло не так');
 }
-//дни недели без if и switch
-showWeek(week[+(lang<='en')]);
 
-let namePerson = 'Cthutq';
+//дни недели без if и switch
+showWeek(weekDeys[lang]);
+
+
+let namePerson = 'Сергей';
 
 (namePerson == "Артем") ? console.log("Директор") : (namePerson == "Александр") ? console.log("Преподаватель") : console.log("Студент");
 
