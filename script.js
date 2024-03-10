8f40517b6e2692b141fb0f676602424f9705f82f
Sergey <sergos5@yandex.ru>
2024-03-10 21:08:42 +0300

    задание урока 7 с иправлениями

--- script.js
+++ script.js
@@ -2,17 +2,19 @@
 
 //Вывести на экран все дни недели
 
-const weekDeys = ['понедельник', 'вторник', 'среда', 'четверг', 'пятница', 'суббота','воскресенье'];
+const weekDeys = ['понедельник', 'вторник', 'среда', 'четверг', 'пятница', 'суббота','воскресенье' ];
 
 const date = new Date();
-const deyNumber = date.getDay();
+let deyNumber = date.getDay();
+let element = document.getElementById("myElement");
 
-
-weekDeys.forEach (function(item, index) {
+weekDeys.forEach (function(item, index) {        
     let dey = item +'<br>';
+    if (deyNumber===0) deyNumber=7;
     if (index+1===deyNumber) dey = '<b>' + dey + '</b>';
     if (index>4) dey = '<i>' + dey + '</i>';
-    document.write(dey);
+    element.insertAdjacentHTML('beforeend', dey);
+
 });
 
 
